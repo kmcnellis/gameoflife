f239799b4d00150eaf7f0ba328f5387226379187
Kiana McNellis <kianam@google.com>
2022-04-14 15:54:37 -0500

    Move test case object inside the test

--- test/test_game_of_life.py
+++ test/test_game_of_life.py
@@ -4,15 +4,6 @@ import contextlib
 
 from src.game_of_life import GameOfLife
 
-
-class TestCase:
-  def __init__(self, desc, pattern, x, y, result):
-    self.desc = desc
-    self.pattern = pattern
-    self.x = x
-    self.y = y
-    self.result = result
-
 class GameOfLifeTest(unittest.TestCase):
 
     def test_run_doesnt_do_much(self):
@@ -28,16 +19,24 @@ class GameOfLifeTest(unittest.TestCase):
         self.assertEqual(output, "I don't do much, yet.\n")
 
     def test_evolve(self):
+        class Case:
+            def __init__(self, desc, pattern, x, y, result):
+                self.desc = desc
+                self.pattern = pattern
+                self.x = x
+                self.y = y
+                self.result = result
+
         game = GameOfLife()
         fake_stdout = io.StringIO()
         tests = [
-            TestCase("single square", ((1, 1)), 1, 1, 0)
+            Case("single square", [(1, 1)], 1, 1, 0)
         ]
         for t in tests:
             # set the state
             game.init(t.pattern)
             # test that the next evolution for a coordinate is correct
-            result = game.evolve(t.x,t.y)
+            result = game.evolve(t.x, t.y)
 
-            print("Test %s, got %d, want %d" %( t.desc, result, t.result))
-            self.assertEqual(t.result,result)
+            print("Test %s, got %d, want %d" % (t.desc, result, t.result))
+            self.assertEqual(t.result, result)
