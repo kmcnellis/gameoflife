e6d8ddb9a18d741da108b205a6c626363328c0bf
Kiana McNellis <kianam@google.com>
2022-04-14 15:49:38 -0500

    add evolve test case:

--- src/game_of_life.py
+++ src/game_of_life.py
@@ -4,5 +4,13 @@ class GameOfLife():
     def run(self):
         print("I don't do much, yet.")
 
+    def init(self,pattern):
+        print("I don't do much, yet.")
+
+    def evolve(self,x,y):
+        print("I don't do much, yet.")
+        return 0
+
+
 if __name__ == "__main__":
     GameOfLife().run()
--- test/test_game_of_life.py
+++ test/test_game_of_life.py
@@ -4,6 +4,15 @@ import contextlib
 
 from src.game_of_life import GameOfLife
 
+
+class TestCase:
+  def __init__(self, desc, pattern, x, y, result):
+    self.desc = desc
+    self.pattern = pattern
+    self.x = x
+    self.y = y
+    self.result = result
+
 class GameOfLifeTest(unittest.TestCase):
 
     def test_run_doesnt_do_much(self):
@@ -17,3 +26,18 @@ class GameOfLifeTest(unittest.TestCase):
         fake_stdout.close()
 
         self.assertEqual(output, "I don't do much, yet.\n")
+
+    def test_evolve(self):
+        game = GameOfLife()
+        fake_stdout = io.StringIO()
+        tests = [
+            TestCase("single square", (1,1), 1,1, 0)
+        ]
+        for t in tests:
+            # set the state
+            game.init(t.pattern)
+            # test that the next evolution for a coordinate is correct
+            result = game.evolve(t.x,t.y)
+
+            print("Test %s, got %d, want %d" %( t.desc, result, t.result))
+            self.assertEqual(t.result,result)
